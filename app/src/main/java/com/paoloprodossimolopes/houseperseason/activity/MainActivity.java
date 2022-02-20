f3fa1ede90bdb004ecf5752e91c1016d233839ef
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-02-19 23:11:25 -0300

    FEATURE (LOGIN): CREATE A LOGIN ACTIVITY
    DESCRIPTION:
    - CREATE LOGIN ACTIVITY
    - ADD SOME TEXTS IN STRING FILE
    - CREATE BG_BUTTON AND BG_EDIT DRAWABLES FILE
    - ORGINIZE FOLDERS
    - CRATE MODEL, ACTIVITY, ADAPTER AND AUTHENTICATION FOLDERS

--- app/src/main/java/com/paoloprodossimolopes/houseperseason/activity/MainActivity.java
+++ app/src/main/java/com/paoloprodossimolopes/houseperseason/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.paoloprodossimolopes.houseperseason;
+package com.paoloprodossimolopes.houseperseason.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.paoloprodossimolopes.houseperseason.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/paoloprodossimolopes/houseperseason/activity/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.paoloprodossimolopes.houseperseason.activity.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.paoloprodossimolopes.houseperseason.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
