b5ac85da8a705c3156ebaa6ca95d4e7c843925d6
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-02-20 12:14:17 -0300

    FEATURE (REGISTER): CREATE A REGISTER LAYOUT
    DESCRIPTION:
    - CREATE FILE AND STYLE INSIDE REGISTRATION ACTIVITY.XML
    - INITIALIZE COMPONENTS IN LOGIN ACTIVITY JAVA
    - CONFIUGURE ACTIONS IN LOGIN ACTIVITY
    - CREATE A TOOL BAR CUSTOM

--- app/src/main/java/com/paoloprodossimolopes/houseperseason/activity/authentication/LoginActivity.java
+++ app/src/main/java/com/paoloprodossimolopes/houseperseason/activity/authentication/LoginActivity.java
@@ -2,15 +2,55 @@ package com.paoloprodossimolopes.houseperseason.activity.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import com.paoloprodossimolopes.houseperseason.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText emailET;
+    private EditText passwordET;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cofigureClicks();
+        configureUIComponents();
     }
+
+    private void configureUIComponents() {
+        emailET = findViewById(R.id.loginScreen_emailEditText);
+        passwordET = findViewById(R.id.loginScreen_passwordEditText);
+    }
+
+    private void cofigureClicks() {
+
+        //forgot password
+        findViewById(R.id.loginScreen_forgotTextView).setOnClickListener(view -> forgotPasswordHandle());
+
+        //enter button
+        findViewById(R.id.loginScreen_loginButton).setOnClickListener(view -> enterButtonHandleTapped());
+
+        //register button
+        findViewById(R.id.loginScreen_createAccountButton).setOnClickListener(view -> goToRegisterActivity());
+    }
+
+    private void goToRegisterActivity() {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+
+    private void enterButtonHandleTapped() {
+
+    }
+
+    private void forgotPasswordHandle() {
+
+    }
+
 }
---
+++ app/src/main/java/com/paoloprodossimolopes/houseperseason/activity/authentication/RegistrationActivity.java
@@ -0,0 +1,19 @@
+package com.paoloprodossimolopes.houseperseason.activity.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.paoloprodossimolopes.houseperseason.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+
+    }
+}
